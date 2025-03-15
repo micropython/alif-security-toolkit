17de33c799537f19ec8b10f9f52afe3ee2bcb0c1
Josuah Demangeon <me@josuah.net>
2025-03-15 17:22:52 +0100

    toolkit/app-write-mram.py: Remove the need to filter the path.
    The configuration file bin/application_package.ds was using a relative
    path, which had to be stripped away from every other location.
    
    This removes the "../" from both the configuration file and the
    "app-write-mrap.py" script
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

--- toolkit/app-write-mram.py
+++ toolkit/app-write-mram.py
@@ -369,14 +369,11 @@ def main():
         arg_list = args.images
 
     if args.skip:
-        idx = arg_list.find("../build/AppTocPackage.bin 0x")
-        arg_list = arg_list[idx : idx + 37]
+        idx = arg_list.find("build/AppTocPackage.bin 0x")
+        arg_list = arg_list[idx : idx + 35]
 
     # validate all parameters
     arg_list = validateArgList(action, arg_list.strip(), args.pad)
-
-    if sys.platform in ["linux", "darwin"]:
-        arg_list = arg_list.replace("../", "")
 
     print("[INFO]", action + arg_list)
 
@@ -506,7 +503,6 @@ def main():
             addr = items[e]
             address = int(addr, base=16)
             fileName = items[e - 1]
-            fileName = fileName.replace("..\\", "")
 
             print("OSPI address verification...")
             if getOspiMemTypeFromAddress(address & 0xF0000000) != "INVALID":
